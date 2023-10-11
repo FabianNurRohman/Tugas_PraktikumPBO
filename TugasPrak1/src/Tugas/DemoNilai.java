bf191db7c3d0ce1c5215483b5cd8d529e5bf78bf
FabianNurRohman <114641083+FabianNurRohman@users.noreply.github.com>
2023-10-11 19:01:57 +0700

    Add files via upload

---
+++ TugasPrak1/src/Tugas/DemoNilai.java
@@ -0,0 +1,18 @@
+package Tugas;
+
+public class DemoNilai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Nilai NMHS = new Nilai();
+		NMHS.nim = "221131318";
+		NMHS.nama = "Mahmud Toriq";
+		NMHS.NAbsen = (float) 90;
+		NMHS.NTugas = (float) 80;
+		NMHS.NUTS = (float) 87;
+		NMHS.NUAS = (float) 85;
+		NMHS.CetakNilai();
+		
+	}
+
+}
